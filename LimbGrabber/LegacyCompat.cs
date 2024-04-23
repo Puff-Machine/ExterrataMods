9b45592dab711ffc6a5695248cec8c7d65024171
Puff Machine <puff@127.0.0.1>
2024-04-23 23:46:50 +0100

    Clean up commented out failures in LegacyCompat.cs

--- LimbGrabber/LegacyCompat.cs
+++ LimbGrabber/LegacyCompat.cs
@@ -8,27 +8,9 @@ using System.Text;
 namespace Koneko;
 internal sealed class LegacyCompat
 {
-    //public static AssemblyBuilder assembly;
-    //public static ModuleBuilder module;
     public static Assembly compatasm = null;
     public static void Initialize()
     {
-        // this did not work, i guess it doesnt try to load the assembly
-        // https://stackoverflow.com/a/16324781
-        // AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolveEventHandler;
-        
-        // this never gets called either
-        //AppDomain.CurrentDomain.TypeResolve += (object sender, ResolveEventArgs args) => { MelonLogger.Msg($"TypeResolve!: {args}"); return null; };
-        
-        // also no
-        /*
-        assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("CVRLimbsGrabber"), AssemblyBuilderAccess.RunAndCollect);
-        module = assembly.DefineDynamicModule("CVRLimbsGrabber");
-        
-        var grabbercomp = module.DefineType("Koneko.GrabberComponent", TypeAttributes.Public, typeof(GrabberComponent));
-        grabbercomp.CreateType();
-        */
-
         Assembly thisasm = Assembly.GetExecutingAssembly();
         Stream compatstream = null;
         try
@@ -59,21 +41,5 @@ internal sealed class LegacyCompat
             }
         }
     }
-
-    /*
-    private static Assembly AssemblyResolveEventHandler(object sender, ResolveEventArgs args)
-    {
-        if (LimbGrabber.Debug.Value)
-        {
-            MelonLogger.Msg($"LimbGrabber AssemblyResolveEventHandler: {args}");
-        }
-        if (args.Name.StartsWith("CVRLimbsGrabber"))
-        {
-            MelonLogger.Msg($"Redirecting failing assembly load for {args.Name} to {Assembly.GetExecutingAssembly().FullName}");
-            return Assembly.GetExecutingAssembly();
-        }
-        return null;
-    }
-    */
 }
 
